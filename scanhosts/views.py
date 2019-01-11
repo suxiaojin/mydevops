6b3bcd9e485f4ffd7465a1eb78bf1c65bf0dc208
suxiaojin <suxj@huiyinxun.com>
2019-01-11 10:57:01 +0800

    20190111

---
+++ scanhosts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from scanhosts.models import *
+import json
+from django.http import JsonResponse,HttpRequest
+
+# Create your views here.
+def user_history():
+    pass
+
+def user_info():
+    pass
